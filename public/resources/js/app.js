ec36d55eb2af47d8474a23cbc1a9a92a8f9f5d19
Leela Mohan <lmohan@salesforce.com>
2020-11-05 16:38:38 +0530

    checkin

--- public/resources/js/app.js
+++ public/resources/js/app.js
@@ -14,6 +14,14 @@ $(document).ready(function () {
 		$('#left2').append('<img src="/resources/images/leftfoot.png">');
 		$('#right1').append('<img src="/resources/images/rightfoot.png">');
 		$('#left1').append('<img src="/resources/images/leftfoot.png">');
+
+		$('#m-right3').append('<img src="/resources/images/rightfoot.png">');
+		$('#m-left3').append('<img src="/resources/images/leftfoot.png">');
+		$('#m-right2').append('<img src="/resources/images/rightfoot.png">');
+		$('#m-left2').append('<img src="/resources/images/leftfoot.png">');
+		$('#m-right1').append('<img src="/resources/images/rightfoot.png">');
+		$('#m-left1').append('<img src="/resources/images/leftfoot.png">');
+
 	}
 
 	function addFootPrintContent(){
@@ -62,6 +70,57 @@ $(document).ready(function () {
 						<div class="footprint-details right3"><ul><li>798 hours <span style="font-weight:100;"> of VTO - 71%</span></li><li>7 Volunteer Rockstars</li></ul></div>
 				
 					</div>
+		`
+		);
+
+		$('#footprint-container-mobile').append(
+			`
+			<div class="footprint-container fc-left1">
+			<div class="footprint left1" id="m-left1"><img src="/resources/images/leftfoot.png"></div>
+			<div class="footprint-left left1">ACV <br/>Influenced</div>
+
+			<div class="footprint-details left1"><ul><li>164M <span style="font-weight:100;"> Influenced in H1</span></li><li> <span style="font-weight:100;">AMER $96.7M, <br/>EMEA $46M, APAC $20.7M  </span></li></ul></div>
+	
+		
+			
+		</div>
+		<div class="footprint-container fc-left1">
+		
+			<div class="footprint right1" id="m-right1"><img src="/resources/images/rightfoot.png"></div>
+			<div class="footprint-right right1">Overall Utilization %</div>
+			<div class="footprint-details right1" ><ul><li>49% onDemand <span style="font-weight:100;"> <br/>and with Overall 109%</span></li></ul></div>
+	
+		</div>
+		<div class="footprint-container fc-left2">
+			<div class="footprint left2" id="m-left2"><img src="/resources/images/leftfoot.png"></div>
+			<div class="footprint-left left2">Demo Components <br/> & Assets</div>
+
+			<div class="footprint-details left2"><ul><li>20 <span style="font-weight:100;"> Demo Components</span></li><li>12 <span style="font-weight:100;"> Demo Assets</span></li><li>9 <span style="font-weight:100;"> Trailhead Rangers</span></li></ul></div>
+	
+		
+		</div>
+		<div class="footprint-container fc-left2">
+		
+			<div class="footprint right2" id="m-right2"><img src="/resources/images/rightfoot.png"></div>
+			<div class="footprint-right right2">App Exchange</div>
+			<div class="footprint-details right2"><ul><li>9 <span style="font-weight:100;"> App Exchange Apps</span></li></ul></div>
+	
+		</div>
+		<div class="footprint-container fc-left3">
+			<div class="footprint left3" id="m-left3"><img src="/resources/images/leftfoot.png"></div>
+			<div class="footprint-left left3">Learnings</div>
+
+			<div class="footprint-details left3"><ul><li>13 <span style="font-weight:100;"> Certifications</span></li><li>9 <span style="font-weight:100;"> Double Star Rangers</span></li><li>9 <span style="font-weight:100;"> Trailhead Rangers</span></li></ul></div>
+	
+		
+		</div>
+		<div class="footprint-container fc-left3">
+		
+			<div class="footprint right3" id="m-right3"><img src="/resources/images/rightfoot.png"></div>
+			<div class="footprint-right right3">VTO</div>
+			<div class="footprint-details right3"><ul><li>798 hours <span style="font-weight:100;"> of VTO - 71%</span></li><li>7 Volunteer Rockstars</li></ul></div>
+	
+		</div>
 		`
 		);
 	}
@@ -287,6 +346,8 @@ $(document).ready(function () {
 			  animateFootPrints();
 		 }, 9000);
 		}
+
+	
 	});
 
 	$('.bg-image').on('mouseover', function (e) {
